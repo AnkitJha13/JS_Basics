bbf1b77584ec8300c73c190fa2368bb398d472f8
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-06 23:11:33 +0530

    Update Higher_order_function.js

--- JS_Basics/Higher_order_function.js
+++ JS_Basics/Higher_order_function.js
@@ -1,4 +1,24 @@
 // Higher order function are those functions which accepth the function as a parameter or return a function or both 
 
 
-// for-each is a hihgher order function 
+// for-each is a higher order function 
+
+
+let a = [1,2,3,4,5]
+
+a.forEach(function(val) {   // output - 3,4,5,6,7
+    console.log(val+2);
+});
+console.log(a);  // [1,2,3,4,5]   // for-each makes changes in temporary copy
+
+// here forEach is a function in which which is accepting function as a parameter here 
+
+
+
+
+
+
+// Higher order functions are used in many places like in setTimeout , setInterval , map , filter , reduce   as all these are functions itself and a function is passed inside them 
+// as a parameter 
+
+// also used when we make closures 
