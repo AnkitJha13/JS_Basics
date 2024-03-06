6021f557501fbcd65293e32aabc8000c58be500b
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-06 20:26:37 +0530

    Create truthy_falsy.js

---
+++ JS_Basics/truthy_falsy.js
@@ -0,0 +1,5 @@
+// falsy - a value is considered "falsy" if it evaluates to false when used in a boolean context
+//falsy values ye hai -> 0 , NULL , NAN , undefined , false, document.all
+
+
+// truthy --> all the no positive or negative or except falsy can be considered truthy
