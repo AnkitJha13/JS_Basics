cec95697e728fac4e0547d439cc6d734efbef7cb
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-06 21:27:53 +0530

    Create Callback_function.js

---
+++ JS_Basics/Callback_function.js
@@ -0,0 +1,10 @@
+// A callback is a function that will be executed aer another function gets executed
+
+
+// setTimeout wala hi example chal jayega callback function mai to 
+
+function greet(){
+    console.log("athena");
+}
+
+setInterval(greet , 2000)
