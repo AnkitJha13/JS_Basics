bba96ce982b53817612137c3d096a8d5075a7e5c
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-06 21:20:52 +0530

    Create setTimeout_Interval.js

---
+++ JS_Basics/setTimeout_Interval.js
@@ -0,0 +1,11 @@
+
+setTimeout(function(){
+    console.log("athena greek god");        // ye ek baar chalega bss 2 sec baad
+},2000)
+
+
+
+
+setInterval(function(){
+    console.log("athena greek god");        // ye har 2 sec ke interval pe chalega 
+},2000)
