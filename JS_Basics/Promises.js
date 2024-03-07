0acce6a4adcfb540fbf8317fe157dc618d64b0e3
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-07 22:55:41 +0530

    Create Promises.js

---
+++ JS_Basics/Promises.js
@@ -0,0 +1,9 @@
+- Promises are used to handle asynchronous operations in javascript.
+
+- A promise is created using the Promise constructor which takes in a callback function with
+  two parameters, resolve and reject respectively.
+
+- resolve is a function that will be called when the async operation has been
+  successfully completed.
+- reject is a function that will be called, when the async operation fails or if some error
+  occurs
