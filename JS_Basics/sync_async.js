17937daff9b4c18528b7807487445b80f9930987
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-07 18:36:40 +0530

    Create sync_async.js

---
+++ JS_Basics/sync_async.js
@@ -0,0 +1,7 @@
+sync
+- It is single threaded 
+- In sync until the first task is completed or executed we cannot start any other task 
+
+async
+- It is multi threaded
+- All the tasks will be running in parallel simultaneously and the one which gets executed first will be returned
