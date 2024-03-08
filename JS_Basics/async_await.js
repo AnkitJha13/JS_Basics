af7c40dff3e2be0f58c547c1381fecd9f8ea2f94
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-08 11:03:20 +0530

    Update async_await.js

--- JS_Basics/async_await.js
+++ JS_Basics/async_await.js
@@ -1,1 +1,23 @@
+// function abcd(){
+//     fetch(`https://randomuser.me/api/`)
+//     .then(function (raw){
+//         return raw.json();
+//     })
+//     .then(function (data){
+//         console.log(data);
+//     })
+// };
 
+// abcd();
+
+
+
+async function abcd(){
+    let raw = await fetch(`https://randomuser.me/api/`)
+                                   // ek await se ek then hata do bss
+    let ans = await raw.json();    // yaha pe await dusre then ke liye hai
+                                   // nhi use karenge to promise return karega agar await use karenge to ans aane pe return karega 
+    console.log(ans);
+};
+
+abcd();
