11c3f8764efcae7d16909d6a9a05b4edf3ca7b33
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-03-08 17:40:38 +0530

    Create map_filter_reduce.js

---
+++ JS_Basics/map_filter_reduce.js
@@ -0,0 +1,60 @@
+// map:-
+It applies a given function on all the elements of the array and returns the updated array
+
+let num = [1,2,3,4,5]
+let ans = num.map(function(value) {
+    return value*3;
+})
+
+console.log(ans);  // output - [3,6,9,12,15]
+
+
+
+
+
+
+// filter:-
+It filters the elements of the array that return false for the applied condition and returns the array which contains elements that satisfy the applied condition. 
+
+let num = [1,2,3,4,5,6,7,8]
+let ans = num.filter(function(value) {
+    return value>3;
+})
+
+console.log(ans);  // output - [4,5,6,7,8]
+
+
+
+
+
+
+
+
+// reduce:-
+It reduces all the elements of the array to a single value by repeatedly applying a function
+let num = [1, 2, 3, 4, 5]
+let ans = num.reduce((acc, val) => {
+    return acc = acc + val;
+},0)
+
+console.log(ans);
+
+
+
+
+
+
+
+
+
+
+
+=> to see each step sum 
+let num = [1, 2, 3, 4, 5];
+let ans = num.reduce((acc, val, index) => {
+    acc = acc + val;
+    console.log(`Step ${index + 1}: Accumulator = ${acc}`);
+    return acc;
+}, 0);
+
+console.log('Final Result:', ans);
