0b348ea3afea46ee52f37166fb9e26ff41b13449
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-06-07 18:04:16 +0530

    Create BOM.js

---
+++ JS_Basics/BOM.js
@@ -0,0 +1,15 @@
+
+# BOM (Browser Object Model)
+- It is a programming interface.
+- It allows users to interact with the browser window and its components such as history, location and navigator objects.
+- DOM, which deals with the document content, the BOM deals with the browser environment.
+
+
+
+
+# window.location.href returns the href (URL) of the current page
+
+# window.history object contains the browsers history.
+
+# window.navigator object contains information about the visitor's browser.
+
