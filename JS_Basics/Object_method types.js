a2365ebb087e085b94ff6b216e9b994279ede179
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-06-07 22:31:20 +0530

    Create Object_method types.js

---
+++ JS_Basics/Object_method types.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+// Using the new keyword 
+
+// var object  = new Object();
+
+
+
+
+// Using Object.create
+
+// var object  = Object.create(null)
+
+
+
+
+
+
+// Using object literal notation (curly braces)
+
+// var object = {
+//     name: "Athena",
+//     age: 22
+// }
+
+
+
+
+
+// Using a constructor function
+
+// function Individual(username,age){
+//     this.username = username,
+//     this.age = age
+// }
+// var object  = new Individual("Athena", 15);
+// console.log(object);
+
+
+
+
+
+// Using a class
+
+// class Individual{
+//  constructor(username){
+//     this.username = username
+//  }
+// }
+// var object  = new Individual("Athena");
