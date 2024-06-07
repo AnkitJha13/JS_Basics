6105831800002c37dea1cac94c3d4ed5618b58ed
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-06-07 17:42:50 +0530

    Update DOM.js

--- JS_Basics/DOM.js
+++ JS_Basics/DOM.js
@@ -1,3 +1,8 @@
+
+# The Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content.
+
+
+
 const a = document.querySelector('h1')
 a.innerHTML = "changed"    // changing HTML
 
