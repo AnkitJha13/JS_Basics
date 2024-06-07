553c6f4b8aa7a39a6e192e1bc5df650171889395
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-06-07 23:24:56 +0530

    Create Object Destructuring.js

---
+++ JS_Basics/Object Destructuring.js
@@ -0,0 +1,37 @@
+# Object destructuring in JavaScript is a syntax that allows you to extract properties from an object and assign them to variables.
+
+
+// without this object destructuring
+
+const book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925
+}
+const title = book.title;
+const author = book.author;
+const year = book.year;
+
+console.log(title);  // Output: The Great Gatsby
+console.log(author); // Output: F. Scott Fitzgerald
+console.log(year);   // Output: 1925
+
+
+
+  
+
+
+
+// Using Object Destructuring
+const book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925
+};
+
+// Destructuring assignment
+const { title, author, year } = book;
+
+console.log(title);  // Output: The Great Gatsby
+console.log(author); // Output: F. Scott Fitzgerald
+console.log(year);   // Output: 1925
