092194159ef3939c214d094826a95bf40bb49d10
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-06-07 18:09:19 +0530

    Update functions.js

--- JS_Basics/functions.js
+++ JS_Basics/functions.js
@@ -8,6 +8,12 @@ abd();
 
 
 // Arrow / lambda function
+
+# Arrow functions were introduced in the ES6 version of javascript.
+# Arrow functions are declared without the function keyword. 
+    
+
+
 abd = () => {
   console.log("athena");
 }
