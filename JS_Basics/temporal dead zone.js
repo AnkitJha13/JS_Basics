26266dfb49f3efab44078fa1f309f3f7a9cdfcc8
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-06-08 10:55:30 +0530

    Create temporal dead zone.js

---
+++ JS_Basics/temporal dead zone.js
@@ -0,0 +1,11 @@
+
+# Temporal Dead Zone is a behaviour that occurs with variables declared using let and const keywords.
+# It is a behaviour where we try to access a variable before it is initialized.
+
+
+
+
+a = 5;
+console.log(a);
+
+let a;     // since decalred using let and const 
