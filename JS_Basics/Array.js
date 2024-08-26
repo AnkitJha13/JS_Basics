93254eb5877e23563bc6b67f83c9a05d22198c6f
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-08-26 09:23:22 +0530

    Update Array.js

--- JS_Basics/Array.js
+++ JS_Basics/Array.js
@@ -1,14 +1,35 @@
 let arr = [1,2,3,4,5]
 
-// arr.push(6)  // [ 1, 2, 3, 4, 5, 6 ] 
+arr.push(6)  // [ 1, 2, 3, 4, 5, 6 ] 
 
-// arr.pop()       // [ 1, 2, 3, 4 ]
+arr.pop()       // [ 1, 2, 3, 4 ]
 
-// arr.splice(2,1);   // [ 1, 2, 4, 5 ]       // basically kitni values hatengi 2nd index se idhar 1 hata hai
-// arr.splice(2,3)    // [ 1, 2 ]
+arr.shift()  // deletes 1st value
 
-// arr.shift()  // deletes 1st value
-
-// arr.unshift(0)  // [ 0, 1, 2, 3, 4, 5 ]    // basically adds the value in the start
+arr.unshift(0)  // [ 0, 1, 2, 3, 4, 5 ]    // basically adds the value in the start
 
 console.log(arr);
+
+
+
+
+
+// In JavaScript, both slice() and splice() are used to manipulate arrays
+
+- slice()
+Purpose: Extracts a portion of an array without modifying the original array
+
+const arr = [1, 2, 3, 4, 5];
+const slicedArr = arr.slice(1, 4); // [2, 3, 4]
+console.log(arr); // [1, 2, 3, 4, 5] (original array remains unchanged)
+
+
+
+- splice()
+Purpose: Adds, removes, or replaces elements in an array.
+
+  
+const arr = [1, 2, 3, 4, 5];
+const splicedArr = arr.splice(1, 2, 10, 11); // [2, 3] (removed elements)
+console.log(arr); // [1, 10, 11, 4, 5] (original array modified)
+
